578d83496550edde92d40b5a4d119902eb76211a
Ryan McLennan <rymclennan@gmail.com>
2016-02-29 17:27:04 -0500

    Initial Commit

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<div class="sidebar">
+	<ul class="xoxo">
+		<li>list item 1</li>
+		<li>this</li>
+		<li>is</li>
+		<li>a</li>
+		<li>thing</li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<?php  dynamic_sidebar( 'primary-widget-area' ); ?>
+	</ul>
+</div>
+	
---
+++ single.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+
+<div class="main">
+  <div class="container">
+    <div class="content">
+    <h1>HEY DUDE</h1>
+    <img src="https://www.placecage.com/c/200/300" alt=""/>
+    <a href="http://nhl.com">Fuckin' Around</a>
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+
+          <div class="entry-meta">
+            <?php hackeryou_posted_on(); ?>
+          </div><!-- .entry-meta -->
+
+          <div class="entry-content">
+            <?php the_content(); ?>
+            <?php wp_link_pages(array(
+              'before' => '<div class="page-link"> Pages: ',
+              'after' => '</div>'
+            )); ?>
+          </div><!-- .entry-content -->
+
+          <div class="entry-utility">
+            <?php hackeryou_posted_in(); ?>
+            <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+          </div><!-- .entry-utility -->
+        </div><!-- #post-## -->
+
+        <div id="nav-below" class="navigation">
+          <p class="nav-previous"><?php previous_post_link('%link', '&larr; %title'); ?></p>
+          <p class="nav-next"><?php next_post_link('%link', '%title &rarr;'); ?></p>
+        </div><!-- #nav-below -->
+
+        <?php comments_template( '', true ); ?>
+
+      <?php endwhile; // end of the loop. ?>
+
+    </div> <!-- /.content -->
+
+    <?php get_sidebar(); ?>
+
+  </div> <!-- /.container -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
